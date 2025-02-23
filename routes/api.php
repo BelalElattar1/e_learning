691bdfbe402b51f2804c06eafc4f0cdcc1afe4ae
Belal Elattar <belal800@gmail.com>
2025-02-24 01:38:06 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTAuthController;
-use App\Http\Middleware\JwtMiddleware;
 
 Route::group(['middleware' => 'JwtAuth'], function () {
 
